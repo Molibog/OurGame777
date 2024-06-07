2cffd2770fd4632fc45b52690709c27d34887093
Pavel <pvltrbn2211@gmail.com>
2024-06-07 11:29:05 +0300

    feat/apdateCard

--- client/src/component/Card/Card.tsx
+++ client/src/component/Card/Card.tsx
@@ -1,70 +1,65 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 function Card() {
-  // const { id } = useParams();
-  // const [question, setQuestion] = useState([]);
+
   const [question, setQuest] = useState({});
-  // async function fetchData() {
-  //   const s = await fetch(`/api/question/${currentTheme}`);
-  //   const r = await s.json();
-  //   setQuestion(r.data);
-  // }
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+
   const [answer, setAnswer] = useState("");
   const [rightAnswer, setRightAnswer] = useState("");
   const { themeId, questionId } = useParams();
 
-  useEffect(() => {
-    fetch(`/api/question/${questionId}`)
-      .then((res) => res.json())
-      .then((data) => setQuest(data.question));
-  }, [themeId, questionId]);
-
-  function chekAnswer() {
-    if (answer === question.answer) {
-      setRightAnswer("Верно!");
-    } else {
-      setRightAnswer(`Правильный ответ: ${question.answer}`);
-    }
+  async function getQuestion() {
+    const respons = await axios.get(`/api/question/${questionId}`)
+    setQuest(respons.data.question)
+    
+    
   }
+  
+  
+    useEffect(() => {
+      getQuestion()
+  
+    }, [themeId, questionId]);
+  
+    function chekAnswer() {
+      if (answer === question.answer) {
+        setRightAnswer("Верно!");
+      } else {
+        setRightAnswer(`Правильный ответ: ${question.answer}`);
+      }
+    }
 
   return (
     <>
       <div className="card" style={{ width: "50rem" }}>
         <h1 className="card-title">{question.title}</h1>
         <img className="card-img-top" src={question.img} />
-        {question.id == 6 || question.id == 12 ? (
-          <div>
-            <input
+        <div>
+        <input
               onChange={(e) => setAnswer(e.target.value)}
               value={answer}
               type="text"
               className="form-control"
               aria-label="Sizing example input"
             />
-            <button onClick={chekAnswer} style={{ color: "blue" }}>
+            <button onClick={()=> chekAnswer()} style={{ color: "blue" }}>
               Проверка
             </button>
+            <h1>{rightAnswer}</h1>
+        </div>
+        {question.id === 6 || question.id === 12 ? (
+          <div>
+
             <Link to={"/"}>В меню</Link>
             <img src="../public/yes-6579_256.gif" alt="" />
           </div>
         ) : (
-          <div>
-            <input
-              onChange={(e) => setAnswer(e.target.value)}
-              value={answer}
-              type="text"
-              className="form-control"
-              aria-label="Sizing example input"
-            />
-            <button onClick={chekAnswer} style={{ color: "blue" }}>
-              Проверка
-            </button>
+          
 
-            <button
+       <div>
+             <button
               onClick={() => {
                 setAnswer("");
                 setRightAnswer("");
@@ -74,8 +69,9 @@ function Card() {
                 Дальше
               </Link>
             </button>
-            <h1>{rightAnswer}</h1>
-          </div>
+          
+       </div>
+      
         )}
       </div>
     </>
